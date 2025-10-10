436b9a796d9f9d40398449fa5b81bc614fd6667e
Nuwandi Ferdinand <it24610801@my.sliit.lk>
2025-10-10 23:26:40 +0530

    BackEnd - MediConnect-Wettasinghe_Pharmaceutical_Distributors

---
+++ Backend/src/test/java/com/example/mediconnectwettasinghe_pharmaceutical_distributors/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediconnectwettasinghe_pharmaceutical_distributors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
